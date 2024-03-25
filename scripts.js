bc068ad6a231056def27615e71393006d83106e8
Grigori Kaplin <82471308+LittleBrotherProg@users.noreply.github.com>
2024-03-25 22:42:58 +0300

    first

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function tambov(){
+    window.location.href = '../tambov/tambov.html'
+}
+function magadan(){
+    window.location.href = '../magadan/magadan.html'
+}
+function mordovia(){
+    window.location.href = '../mordovia/mord.html'
+}
+function ingush(){
+    window.location.href = '../ingush/ingysh.html'
+}
+function IN(){
+    window.location.href = '../IN/main.html'
+}
+function main(){
+    window.location.href = '../main.html'
+}
